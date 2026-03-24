5ea3f9a11d74a1ee6237c987a095dc3af637f0bf
jude <jude.segundera@gmail.com>
2026-03-24 02:19:44 -0700

    Updated app theme to red, added EkhoGo logo to top bar, moved Messaging functionality into MessagesScreen.kt to reduce clutter in HomeScreen.kt.

--- app/src/main/java/com/example/ekhogo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/ekhogo/ui/theme/Theme.kt
@@ -1,54 +1,53 @@
 package com.example.ekhogo.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Red80,
+    secondary = RedGrey80,
+    tertiary = Rose80,
+    background = Color(0xFF1A1111),
+    surface = Color(0xFF1A1111),
+    primaryContainer = Red40,
+    secondaryContainer = MessageBubbleGrey,
+    tertiaryContainer = Rose40,
+    onPrimary = Color(0xFF690005),
+    onSecondary = Color(0xFF2C1513),
+    onTertiary = Color(0xFF3B0908),
+    onBackground = Color(0xFFFFEDEA),
+    onSurface = Color(0xFFFFEDEA),
+    onPrimaryContainer = Color.White,
+    onSecondaryContainer = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Red40,
+    secondary = RedGrey80,
+    tertiary = Rose40,
+    background = RedSurface,
+    surface = RedSurface,
+    primaryContainer = Red40,
+    secondaryContainer = MessageBubbleGrey,
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onBackground = RedOnSurface,
+    onSurface = RedOnSurface,
+    onPrimaryContainer = Color.White,
+    onSecondaryContainer = Color.White
 )
 
 @Composable
 fun EkhoGoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,

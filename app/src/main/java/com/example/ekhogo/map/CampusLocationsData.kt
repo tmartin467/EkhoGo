d3f3b96c495a781ea0609088fa0313d1309550aa
Chris Hernandez <christopher.hernandez563@myci.csuci.edu>
2026-04-21 22:37:57 -0700

    Implement campus map filters and color-coded markers

--- app/src/main/java/com/example/ekhogo/map/CampusLocationsData.kt
+++ app/src/main/java/com/example/ekhogo/map/CampusLocationsData.kt
@@ -4,28 +4,168 @@ import com.google.android.gms.maps.model.LatLng
 
 val campusLocations = listOf(
     CampusLocation(
-        name = "Broome Library",
-        latLng = LatLng(34.1633, -119.0412),
-        description = "John Spoor Broome Library"
+        name = "Coastal Cup",
+        latLng = LatLng(34.16464, -119.04500),
+        description = "Campus Dining"
+    ),
+    CampusLocation(
+        name = "Ekho's Cafe",
+        latLng = LatLng(34.16146, -119.04459),
+        description = "Campus Dining"
+    ),
+    CampusLocation(
+        name = "Sea Store",
+        latLng = LatLng(34.16137, -119.04404),
+        description = "Campus Dining"
     ),
     CampusLocation(
         name = "Islands Cafe",
-        latLng = LatLng(34.1608, -119.0413),
-        description = "Campus dining"
+        latLng = LatLng(34.16044, -119.04158),
+        description = "Campus Dining"
     ),
     CampusLocation(
-        name = "Lindero Hall",
-        latLng = LatLng(34.1600, -119.0409),
-        description = "Academic building"
+        name = "American Pie Company",
+        latLng = LatLng(34.16321, -119.03941),
+        description = "Campus Dining"
+    ),
+    CampusLocation(
+        name = "Tortillas Grill",
+        latLng = LatLng(34.16304, -119.03935),
+        description = "Campus Dining"
+    ),
+    CampusLocation(
+        name = "Mom Wong Kitchen",
+        latLng = LatLng(34.16280, -119.03934),
+        description = "Campus Dining"
+    ),
+    CampusLocation(
+        name = "Gateway Hall",
+        latLng = LatLng(34.16474, -119.04545),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "Shasta Hall",
+        latLng = LatLng(34.16457, -119.04470),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "El Dorado Hall",
+        latLng = LatLng(34.16422, -119.04715),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "Napa Hall",
+        latLng = LatLng(34.16370, -119.04565),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "Del Norte Hall",
+        latLng = LatLng(34.16317, -119.04408),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "MVS Hall",
+        latLng = LatLng(34.16271, -119.04462),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "Sierra Hall",
+        latLng = LatLng(34.16227, -119.04460),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "Aliso Hall",
+        latLng = LatLng(34.16095, -119.04528),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "Ojai Hall",
+        latLng = LatLng(34.16168, -119.04253),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "Malibu Hall",
+        latLng = LatLng(34.16122, -119.04101),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "Bell Tower",
+        latLng = LatLng(34.16097, -119.04313),
+        description = "Academic Building"
+    ),
+    CampusLocation(
+        name = "Topanga Hall",
+        latLng = LatLng(34.16013, -119.04166),
+        description = "Academic Building"
     ),
     CampusLocation(
         name = "Santa Rosa Village",
-        latLng = LatLng(34.1592, -119.0418),
-        description = "Student housing"
+        latLng = LatLng(34.15904, -119.0428),
+        description = "Student Housing"
+    ),
+    CampusLocation(
+        name = "Santa Cruz Village",
+        latLng = LatLng(34.16011, -119.04387),
+        description = "Student Housing"
+    ),
+    CampusLocation(
+        name = "Anacapa Village",
+        latLng = LatLng(34.15919, -119.04485),
+        description = "Student Housing"
+    ),
+    CampusLocation(
+        name = "Town Center",
+        latLng = LatLng(34.16318, -119.03947),
+        description = "Student Housing"
     ),
     CampusLocation(
         name = "Recreation Center",
-        latLng = LatLng(34.1607, -119.0446),
-        description = "Fitness and recreation"
-    )
+        latLng = LatLng(34.16033, -119.04479),
+        description = "Student Services"
+    ),
+    CampusLocation(
+        name = "Police Station",
+        latLng = LatLng(34.16334, -119.04315),
+        description = "Student Services"
+    ),
+    CampusLocation(
+        name = "Student Health Services",
+        latLng = LatLng(34.16401, -119.04109),
+        description = "Student Services"
+    ),
+    CampusLocation(
+        name = "The Cove Bookstore",
+        latLng = LatLng(34.16331, -119.03930),
+        description = "Student Services"
+    ),
+    CampusLocation(
+        name = "John Spoor Broome Library",
+        latLng = LatLng(34.16272, -119.04094),
+        description = "Student Services"
+    ),
+    CampusLocation(
+        name = "Learning Resource Center",
+        latLng = LatLng(34.16261, -119.04068),
+        description = "Student Services"
+    ),
+    CampusLocation(
+        name = "Writing and Multiliteracy Center",
+        latLng = LatLng(34.16284, -119.04050),
+        description = "Student Services"
+    ),
+    CampusLocation(
+        name = "Academic Advising",
+        latLng = LatLng(34.16120, -119.04292),
+        description = "Student Services"
+    ),
+    CampusLocation(
+        name = "Career Center",
+        latLng = LatLng(34.16122, -119.04334),
+        description = "Student Services"
+    ),
+    CampusLocation(
+        name = "VCTC Bus Stop",
+        latLng = LatLng(34.16545, -119.04408),
+        description = "Student Services"
+    ),
 )
--- app/src/main/java/com/example/ekhogo/map/CampusMap.kt
+++ app/src/main/java/com/example/ekhogo/map/CampusMap.kt
@@ -5,10 +5,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
+
+fun getMarkerColor(category: String): Float {
+    return when (category) {
+        "Campus Dining" -> BitmapDescriptorFactory.HUE_ORANGE
+        "Academic Building" -> BitmapDescriptorFactory.HUE_RED
+        "Student Housing" -> BitmapDescriptorFactory.HUE_VIOLET
+        "Student Services" -> BitmapDescriptorFactory.HUE_AZURE
+        else -> BitmapDescriptorFactory.HUE_ROSE
+    }
+}
 
 @Composable
 fun CampusMap(
@@ -17,8 +28,7 @@ fun CampusMap(
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier
-            .fillMaxWidth()
+        modifier = modifier.fillMaxWidth()
     ) {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
@@ -28,11 +38,12 @@ fun CampusMap(
                 Marker(
                     state = MarkerState(position = location.latLng),
                     title = location.name,
-                    snippet = location.description
+                    snippet = location.description,
+                    icon = BitmapDescriptorFactory.defaultMarker(
+                        getMarkerColor(location.description)
+                    )
                 )
             }
         }
     }
 }
-
-

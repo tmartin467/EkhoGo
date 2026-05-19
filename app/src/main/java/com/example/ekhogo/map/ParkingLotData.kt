0c9b4d8e62acbf09ec7b87120d2c62c9b30d91df
jude <jude.segundera@gmail.com>
2026-05-19 14:13:32 -0700

    Campus parking lot pin locations

--- app/src/main/java/com/example/ekhogo/map/ParkingLotData.kt
+++ app/src/main/java/com/example/ekhogo/map/ParkingLotData.kt
@@ -3,11 +3,11 @@ package com.example.ekhogo.map
 import com.google.android.gms.maps.model.LatLng
 
 val parkingLotLocations = listOf(
-    CampusLocation("Lot A1", LatLng(34.16395, -119.04328), "Parking Lots"),
+    CampusLocation("Lot A1", LatLng(34.16356037787434, -119.042674228549), "Parking Lots"),
     CampusLocation("Lot A3", LatLng(34.16613, -119.04719), "Parking Lots"),
     CampusLocation("Lot A3 Gravel", LatLng(34.16719, -119.04803), "Parking Lots"),
     CampusLocation("Lot A4", LatLng(34.16435, -119.04740), "Parking Lots"),
-    CampusLocation("Lot A8", LatLng(34.16288, -119.03972), "Parking Lots"),
-    CampusLocation("Lot A10", LatLng(34.15886, -119.04031), "Parking Lots"),
+    CampusLocation("Lot A8", LatLng(34.16303216821823, -119.04031388461591), "Parking Lots"),
+    CampusLocation("Lot A10", LatLng(34.15930131599958, -119.04060021042822), "Parking Lots"),
     CampusLocation("Lot A11", LatLng(34.16506, -119.04608), "Parking Lots")
 )
